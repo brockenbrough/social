9ebdd42789746e7a5aa771af444c214eeffdf6cf
Allan Brockenbrough <brockenbrough@gmail.com>
2022-10-18 08:28:54 -0400

    Changing port number for project-notes service.

--- project-notes/server/server.js
+++ project-notes/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8095;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/project_notes"));
