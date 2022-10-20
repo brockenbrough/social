febcb4a95b2084b5d6f180fb7d59e0e259959110
rkanoff <r.canough@gmail.com>
2022-10-20 12:39:32 -0400

    Added returnAll backend function

--- project-notes/userservice/server/server.js
+++ project-notes/userservice/server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 const loginRoute = require('./routes/user.login')
+const getAllUsersRoute = require('./routes/user.returnAll')
 const registerRoute = require('./routes/user.signup')
 const dbConnection = require('./config/db.config')
 require('dotenv').config();
@@ -12,6 +13,7 @@ app.use(cors({origin: '*'}))
 app.use(express.json())
 app.use('/user', loginRoute)
 app.use('/user', registerRoute)
+app.use('/user', getAllUsersRoute)
 
 app.listen(SERVER_PORT, (req, res) => {
     console.log(`server is listening on port ${SERVER_PORT}`);
