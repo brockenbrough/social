f5ba2de611a549025d8836d4274e6ec766d757ef
Enoc Gonzalez <enocgonzalez@Enocs-MacBook-Pro.local>
2022-10-21 14:27:39 -0400

    Implementation of Get User By Id

--- userservice/server/server.js
+++ userservice/server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const loginRoute = require('./routes/user.login')
 const getAllUsersRoute = require('./routes/user.returnAll')
 const registerRoute = require('./routes/user.signup')
+const retrieveUser = require('./routes/user.getUser')
 const dbConnection = require('./config/db.config')
 const editUser = require('./routes/user.editUser')
 
@@ -17,6 +18,7 @@ app.use('/user', loginRoute)
 app.use('/user', registerRoute)
 app.use('/user', getAllUsersRoute)
 app.use('/user', editUser)
+app.use('/user', retrieveUser)
 
 app.listen(SERVER_PORT, (req, res) => {
     console.log(`server is listening on port ${SERVER_PORT}`);
