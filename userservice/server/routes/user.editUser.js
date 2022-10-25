c4844186b9a2fd452718485760212f09143d5abc
Monse Evelyn <77801300+ERODRIGUEZATSSU@users.noreply.github.com>
2022-10-25 11:03:57 -0400

    This is Eve's change

--- userservice/server/routes/user.editUser.js
+++ userservice/server/routes/user.editUser.js
@@ -18,57 +18,57 @@ router.post('/editUser', async (req, res) =>
         return res.json(user)
     }
 });
-    // if (error) return res.status(400).send({ message: error.errors[0].message });
+ if (error) return res.status(400).send({ message: error.errors[0].message });
 
-    // const {username, email, password} = req.body
+    const {username, email, password} = req.body
 
     // // edit username
-    //    const createUsername = new newUserModel({
-    //     newUsername: req.body.newUsername,
-    //     email: req.body.email,
-    //     password: req.body.hashPassword,
-    // });
-    // try {
-    //     const saveNewUsername = await createUsername.save();
-    //     res.send(saveNewUsername);
-    // } catch (error) {
-    //     res.status(400).send({ message: "Error trying to use new username" });
-    // }
+       const createUsername = new newUserModel({
+         newUsername: req.body.newUsername,
+         email: req.body.email,
+         password: req.body.hashPassword,
+     });
+    try {
+        const saveNewUsername = await createUsername.save();
+        res.send(saveNewUsername);
+    } catch (error) {
+         res.status(400).send({ message: "Error trying to use new username" });
+     }
 
 
     // //edit email //
-    // const newEmail = new newUserModel({
-    //     username: req.body.username,
-    //     newEmail: req.body.newEmail,
-    //     password: hashPassword,
-    // });
-    // try {
-    //     const saveNewEmail = await saveNewEmail.save();
-    //     res.send(saveNewEmail);
-    // } catch (error) {
-    //     res.status(400).send({ message: "Error trying to use new email address" });
-    // }
+     const newEmail = new newUserModel({
+         username: req.body.username,
+         newEmail: req.body.newEmail,
+         password: hashPassword,
+     });
+     try {
+         const saveNewEmail = await saveNewEmail.save();
+        res.send(saveNewEmail);
+     } catch (error) {
+         res.status(400).send({ message: "Error trying to use new email address" });
+     }
 
 
     // // edit password //
-    // const newPassword = new newUserModel({
-    //     username: req.body.username,
-    //     email: req.body.email,
-    //     newPassword: req.body.hashPassword,
-    // });
+     const newPassword = new newUserModel({
+        username: req.body.username,
+         email: req.body.email,
+         newPassword: req.body.hashPassword,
+     });
 
    
-    // try {
-    //     const saveNewPassword = await saveNewPassword.save();
-    //     res.send(saveNewPassword);
-    // } catch (error) {
-    //     res.status(400).send({ message: "Error editting new password, come up withs something better NOOB" });
-    // }
-    // //generates the hash
-    // const generateHash = await bcrypt.genSalt(Number(10))
+     try {
+         const saveNewPassword = await saveNewPassword.save();
+         res.send(saveNewPassword);
+     } catch (error) {
+         res.status(400).send({ message: "Error editting new password, come up withs something better NOOB" });
+     }
+     //generates the hash
+     const generateHash = await bcrypt.genSalt(Number(10))
 
-    //  //parse the generated hash into the password
-    // const hashPassword = await bcrypt.hash(newPassword, generateHash)
+      //parse the generated hash into the password
+     const hashPassword = await bcrypt.hash(newPassword, generateHash)
 })
 
 module.exports = router;
