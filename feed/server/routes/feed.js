2a11c05320496d64441069b05d3ab6dacea0fd05
JWoodSSU <j_wood11@salemstate.edu>
2022-10-25 13:23:07 -0400

    updated feed.js so that it will return posts from the post service

--- feed/server/routes/feed.js
+++ feed/server/routes/feed.js
@@ -1,28 +1,11 @@
 const express = require("express");
-
-// feedRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /project_notes.
-const feedRoutes = express.Router();
-
-// This will help us connect to the database
-const dbo = require("../db/conn");
-
-// This help convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
-
+const axios = require("axios");
+const router = express.Router();
 
 // This section will help you get a list of all the posts.
-feedRoutes.route("/feed").get(function (req, res) {
-  let db_connect = dbo.getDb("feed");
-  db_connect
-    .collection("feed")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
+router.route("/feed").get(async function (req, res) {
+  const posts = await axios.get("http://localhost:8083/posts/post").then(e=>e.data);
+  res.json(posts);
 });
 
-
-module.exports = feedRoutes;
+module.exports = router;
