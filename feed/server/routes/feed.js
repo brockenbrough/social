3a40c5db3edc87d3b1491ab1c5174cf91f2dabb5
JWoodSSU <j_wood11@salemstate.edu>
2022-10-25 16:59:41 -0400

    Updated feed.js to output a properly formatted json with only post ids

--- feed/server/routes/feed.js
+++ feed/server/routes/feed.js
@@ -4,8 +4,16 @@ const router = express.Router();
 
 // This section will help you get a list of all the posts.
 router.route("/feed").get(async function (req, res) {
-  const posts = await axios.get("http://localhost:8083/posts/post").then(e=>e.data);
-  res.json(posts);
+  const response = await axios.get("http://localhost:8083/posts/post");
+  const posts = response.data;
+  var ids = [];
+  for (i = 0; i < posts.length; i++) {
+    ids[i] = posts[i]._id;
+  }
+  let obj = {
+    "feed" : ids
+  }
+  res.json(obj);
 });
 
 module.exports = router;
