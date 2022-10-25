9587fda9b338da322c424e9fb2896083dc3a2e9c
hzitouniATSSU <77801461+hzitouniATSSU@users.noreply.github.com>
2022-10-25 10:54:06 -0700

    add deleteLike function to backend.

--- statistics/server/routes/statistics.js
+++ statistics/server/routes/statistics.js
@@ -61,7 +61,16 @@ statisticsRoutes.route("/statistics/:userID/:postID").post(function (req, respon
     response.json(res);
   });
 });
-
+// This section will help you delete a like
+statisticsRoutes.route("/statistics/:userID/likes/:postID").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect.collection("likes").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
 
 module.exports = statisticsRoutes;
 
