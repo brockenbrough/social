36e4e64c6776e1024c710cc83faa3d11d1d6e49e
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 13:31:31 -0400

    IDK, everything changed

--- following/server/server.js
+++ following/server/server.js
@@ -3,17 +3,16 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 8085;
+const followRoute = require("./routes/following")
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/following"));
+app.use("/", followRoute);
 // get driver connection
 const dbo = require("./db/conn");
+const mongoose = require("mongoose");
  
 app.listen(port, () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
+  mongoose.connect(process.env.ATLAS_URI)
   console.log(`Server is running on port: ${port}`);
 });
