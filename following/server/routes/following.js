5aa687c909f19b928ddad4fa7326883abbcf349f
Jackson Davis <jdaviskid27@yahoo.com>
2022-10-31 20:44:20 -0400

    Reverted it. I'm keeping it the old way.

--- following/server/routes/following.js
+++ following/server/routes/following.js
@@ -18,17 +18,11 @@ const followerModel = require('../model/followerModel')
 
 
 // This section will help you get a list of all the followers.
-followerRoutes.get("/followers", async (req, res) => {
-  const followers = await followerModel.find()
-console.log("fedfeef")
-  return res.json(followers)
-})
-  
 
 followerRoutes.route("/following").get(function (req, res) {
   let db_connect = dbo.getDb("following");
   db_connect
-    .collection("followees")
+    .collection("followers")
     .find({})
     .toArray(function (err, result) {
       if (err) throw err;
