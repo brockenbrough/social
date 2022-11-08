5b6707f64c1e75679578a0dd831b0cdd11b14d38
Joseph Frongillo <j_frongillo@salemstate.edu>
2022-11-08 13:12:57 -0500

    Adding functionalities for displaying all likes and searching by userId.

--- statistics/server/routes/statistics.js
+++ statistics/server/routes/statistics.js
@@ -3,7 +3,7 @@ const route = express.Router();
 
 const likeSchema = require("../models/like");
 
-
+//Alows a user to like a post
 route.post('/likes', async(req,res) => {
   const now = new Date()
   //Creating a timestamp object to pass to 
@@ -20,6 +20,35 @@ route.post('/likes', async(req,res) => {
     res.status(400).send({ message: "Error trying to create new Like" });
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//returns a list of all likes
+route.get('/likes', async(req,res) => {
+  const likes = await likeSchema.find()
+  return res.json(likes)
+})
+
+//returns a list of posts that an individual user liked
+route.get('/likes/:userId', async(req,res) => {
+  const likes = await likeSchema.find({userId: req.params.userId}) 
+  return res.json(likes)
+})
 
 
 module.exports = route;
