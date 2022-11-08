1dc44b899e20f37f4fdd52da62ad32fd16f42d30
Joseph Frongillo <j_frongillo@salemstate.edu>
2022-11-08 10:39:50 -0500

    Fixing like functionality for mongoose.

--- statistics/server/models/like.js
+++ statistics/server/models/like.js
@@ -1,19 +1,9 @@
 const mongoose = require("mongoose");
 
-const like = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true,
-        label: "userId",
-    },
-    postId: {
-        type: String,
-        required: true,
-        label: "postId",
-    },
-    date: {
-        type: Date, 
-        required: true,
-        label: "dateOfLike"
-    }
+const schema = new mongoose.Schema({
+    userId: String,
+    postId: String,
+    date: Date,
 })
+
+module.exports = mongoose.model("Like", schema)
