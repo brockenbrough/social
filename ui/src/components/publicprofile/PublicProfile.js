5cf4a09cdf99542c339422444c96c9d4d36c7a30
Monse Evelyn <77801300+ERODRIGUEZATSSU@users.noreply.github.com>
2022-11-10 12:55:34 -0500

    Trying again

--- ui/src/components/publicprofile/PublicProfile.js
+++ ui/src/components/publicprofile/PublicProfile.js
@@ -4,23 +4,10 @@ import axios from 'axios'
 
 const PublicProfile = () => {
   
-  return (
-    <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Public Profile</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-  </div>
-</nav> )
+  return(
+    <h1> "HEY eve"</h1>
+  
+    )
     
 
   }
