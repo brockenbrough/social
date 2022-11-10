4d4ba9ff5cbe66b6f8bdcb12ba5d1c7dfa527177
Allan Brockenbrough <brockenbrough@gmail.com>
2022-11-10 12:35:46 -0500

    CHanged project-notes to mongoose

--- project-notes/server/db/conn.js
+++ project-notes/server/db/conn.js
@@ -1,28 +1,21 @@
 
-const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const mongoose = require('mongoose');
+const db = process.env.ATLAS_URI;
 
-var _db;
-
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db,
       {
-        _db = db.db("social");
-        console.log("Successfully connected to MongoDB."); 
+        useNewUrlParser: true
       }
-      return callback(err);
-         });
-  },
+    );
 
-  // getDb returns a handle to the database that let's us execute queries.
-  getDb: function () {
-    return _db;
-  },
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
+
+module.exports = connectDB;
