1fda02d8c2d3546880b0b4d34002778a4d74bc7d
Viky Belina <v_belina@salemstate.edu>
2022-11-10 12:46:28 -0500

    Added a Profile button, but it's not connected yet

--- ui/src/components/navbar.js
+++ ui/src/components/navbar.js
@@ -39,6 +39,16 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Profile
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
