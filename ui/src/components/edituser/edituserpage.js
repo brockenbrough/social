7d6967b31306cfd29047c391dd59167aa4fee80f
rkanoff <r.canough@gmail.com>
2022-11-13 15:35:49 -0500

    Added react-bootstrap dependency to correct file and removed from incorrect file

--- ui/src/components/edituser/edituserpage.js
+++ ui/src/components/edituser/edituserpage.js
@@ -1,9 +1,35 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const EditUserPage = () =>{
-    return(
-        <h1>"Hello!"</h1>
-    )
+const EditUserPage = ({children}) =>{
+    const [show, toggleShow] = useState(true);
+return(
+  <Form>
+  <Form.Group className="mb-3" controlId="email">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter new email" />
+    <Form.Text className="text-muted">
+      Placeholder.
+    </Form.Text>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="username">
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="username" placeholder="Enter new username" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="password">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Enter new password" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+  <Button variant="primary" type="cancel">
+    Cancel
+  </Button>
+</Form>
+)
+
 }
 
-export default EditUserPage
+export default EditUserPage;
