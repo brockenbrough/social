c463c4f4a872194b311571e1990aec57a6410bd1
Monse Evelyn <77801300+ERODRIGUEZATSSU@users.noreply.github.com>
2022-11-15 12:05:48 -0500

    public profile changes

--- ui/src/components/publicprofile/PublicProfile.js
+++ ui/src/components/publicprofile/PublicProfile.js
@@ -1,14 +1,56 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Button from "react-bootstrap/Button";
+import './publicprofile.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Form} from 'react-bootstrap';
+import {Image} from "react-bootstrap"
+
+
 
 const PublicProfile = () => {
-  
-  return(
-    <h1> "HEY eve"</h1>
-  
-    )
-    
+  const [show, toggleShow] = useState(true);
+  return (
+    <div className= "Public Page">
+    <header className = "Public-Header">
+    <Form>
+      <Form.Label>
+      Social Media App
+      </Form.Label>
+          <h1> Public Profile Page</h1>
+           <Form.Group controlID = "formUsername" class = "form-control">
+           <Form.Label>
+            Search
+            <Form.Control type = "name" placeholder = "evilynincorportated" class = "form-control"/>
+           </Form.Label>
+          </Form.Group>
+ 
+    <body>
+    <Form.Label>
+      Followers             
+      </Form.Label>
+      <Form.Label>
+      Following
+      </Form.Label>
+      
+    </body>
+        
+     <small>    
+      <Button variant = "primary"> Login </Button>
+     </small>
+    </Form>
+     
+    </header>
+    </div>
 
-  }
+   
+     
+  )
+}
 
-export default PublicProfile
+
+
+
+
+
+export default PublicProfile;
