16aa3efe190754ef2370c2772afac12af76c29fc
JFrongillo <j_frongillo@salemstate.edu>
2022-11-14 22:04:12 -0500

    Adding a check to like post to detect if user already liked the post, and adding a like delete function

--- statistics/server/routes/statistics.js
+++ statistics/server/routes/statistics.js
@@ -30,11 +30,16 @@ route.post('/likes', async(req,res) => {
     date: now,
   };
 
-  try{
-   const response =  await likeSchema.create(userLike);
-    res.send(response);
-  } catch { 
-    res.status(400).send({ message: "Error trying to create new Like" });
+  const likes = await likeSchema.findOne({userId: userLike.userId, postId: userLike.postId})
+  console.log(likes)
+  /**
+   * Checking to see if the like exists in the database, if so, return a 404
+   */
+  if(likes){
+    return res.status(404).json("Post has been liked already.")
+  } else{
+    const response = await likeSchema.create(userLike)
+    return res.status(200).json(response)
   }
 });
 
@@ -42,13 +47,24 @@ route.post('/likes', async(req,res) => {
 route.get('/likes', async(req,res) => {
   const likes = await likeSchema.find()
   return res.json(likes)
-})
+});
 
 //returns a list of posts that an individual user liked
 route.get('/likes/:userId', async(req,res) => {
   const likes = await likeSchema.find({userId: req.params.userId}) 
   return res.json(likes)
-})
+});
+
+//Function to unlike a post. 
+route.delete('/likes/unLike', async(req,res) =>{
+  try{
+    const response = await likeSchema.deleteOne({userId: req.body.userId , postId : req.body.postId})
+    res.status(200).json(response)
+    console.log("Like deleted")
+  }catch{
+    res.status(400).send({message: "Like does not exist"})
+  }
+});
 
 
 /**
@@ -63,7 +79,7 @@ route.get('/count/likes-by-user/:userId', async(req,res) => {
   }catch{
     res.sendStatus(404).send({message: "User does not exist."})
   }
-})
+});
 
 //Find the amount of likes by postId
 route.get('/count/likes-for-post/:postId', async(req,res) => {
@@ -73,7 +89,7 @@ route.get('/count/likes-for-post/:postId', async(req,res) => {
   }catch{
     res.sendStatus(404).send({message: "Post does not exist"})
   }
-})
+});
 
 module.exports = route;
 
