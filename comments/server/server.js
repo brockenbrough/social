c5c33abeef5515ada9474546bed38c5dcc5b0f46
Evan Bun <e_bun@salemstate.edu>
2022-11-15 13:27:24 -0500

    Conversion to mongoose, , missing replyt function

--- comments/server/server.js
+++ comments/server/server.js
@@ -7,13 +7,11 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes/comments"));
 // get driver connection
-const dbo = require("./db/conn");
+const connectDB = require("./db/conn");
  
+// Connect database.
+connectDB();
+
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
   console.log(`Server is running on port: ${port}`);
 });
