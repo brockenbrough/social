8d2957161b99bed64637c34526f6401e4400e630
Joseph Frongillo <j_frongillo@salemstate.edu>
2022-11-17 13:02:04 -0500

    removing path to resolve crash

--- ui/src/App.js
+++ ui/src/App.js
@@ -35,7 +35,6 @@ const App = () => {
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/edituserpage" element={<EditUserPage />} />
         <Route path="/privateUserProfile" element={<PrivateUserProfile />} />
-        <Route path="/privateUserLikeList/" element={<PrivateUserLikeList />}/>
         <Route path="/project-notes/contributors" element={<ContributorList />} />
         <Route path="/project-notes/editContributor/:id" element={<EditContributor />} />
         <Route path="/project-notes/create" element={<CreateContributor />} />
