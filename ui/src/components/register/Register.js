10768811900c3c4857217233c8752c24ba150474
DON <don@DONs-Laptop.local>
2022-11-29 01:08:16 -0500

    added register page

--- ui/src/components/register/Register.js
+++ ui/src/components/register/Register.js
@@ -1,14 +1,46 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import "./register.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import getUserInfo from "../../utilities/decodeJwt";
 
+const PRIMARY_COLOR = "#cc5c99";
+const SECONDARY_COLOR = "#0c0c1f";
+const url = "http://localhost:8081/user/signup";
 const Register = () => {
   const [data, setData] = useState({ username: "", email: "", password: "" });
   const [error, setError] = useState("");
   const navigate = useNavigate();
-  const url = "http://localhost:8081/user/signup";
+  const [light, setLight] = useState(false);
+  const [bgColor, setBgColor] = useState(SECONDARY_COLOR);
+  const [bgText, setBgText] = useState("Light Mode");
+
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.name]: input.value });
+  };
+
+  useEffect(() => {
+    if (light) {
+      setBgColor("white");
+      setBgText("Dark mode");
+    } else {
+      setBgColor(SECONDARY_COLOR);
+      setBgText("Light mode");
+    }
+  }, [light]);
+
+  let labelStyling = {
+    color: PRIMARY_COLOR,
+    fontWeight: "bold",
+    textDecoration: "none",
+  };
+  let backgroundStyling = { background: bgColor };
+  let buttonStyling = {
+    background: PRIMARY_COLOR,
+    borderStyle: "none",
+    color: bgColor,
   };
 
   const handleSubmit = async (e) => {
@@ -16,7 +48,7 @@ const Register = () => {
     try {
       const { data: res } = await axios.post(url, data);
       // const {accessToken} = res
-      //store token in localStorage 
+      //store token in localStorage
       navigate("/login");
     } catch (error) {
       if (
@@ -30,26 +62,105 @@ const Register = () => {
   };
 
   return (
-    <div className="signup">
-      <div>
-        <label>Username: </label>
-        <input type='username' id='username' name='username' onChange={handleChange} />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type='email'  id='email' name='email' onChange={handleChange} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type='password'  name='password' onChange={handleChange} />
-      </div>
-      <div className="error-field">
-        {error && <div>{error}</div>}
-      </div>
-      <input type="submit" value="Signup" onClick={handleSubmit} />
-      <Link to='/'><span><h4>Go Back</h4></span></Link>
-    </div>
-  )
-}
+    <>
+      <section className="vh-100">
+        <div className="container-fluid h-custom vh-100">
+          <div
+            className="row d-flex justify-content-center align-items-center h-100 "
+            style={backgroundStyling}
+          >
+            <div className="col-md-9 col-lg-6 col-xl-5">
+              <img
+                src="https://cdn.dribbble.com/users/1080850/screenshots/11585265/media/01aaa0e9a2325d1032112e98d5c78474.png?compress=1&resize=700x600"
+                className="img-fluid"
+                alt="test image"
+              />
+            </div>
+            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label style={labelStyling}>Username</Form.Label>
+                  <Form.Control
+                    type="username"
+                    name="username"
+                    onChange={handleChange}
+                    placeholder="Enter username"
+                  />
+                  <Form.Text className="text-muted">
+                    We just might sell your data
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label style={labelStyling}>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    placeholder="Enter Email Please"
+                  />
+                  <Form.Text className="text-muted">
+                    We just might sell your data
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label style={labelStyling}>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Text className="text-muted pt-1">
+                    Dont have an account?
+                    <span>
+                      <Link to="/signup" style={labelStyling}>
+                        {" "}
+                        Sign up
+                      </Link>
+                    </span>
+                  </Form.Text>
+                </Form.Group>
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                    onChange={() => {
+                      setLight(!light);
+                    }}
+                  />
+                  <label
+                    class="form-check-label"
+                    for="flexSwitchCheckDefault"
+                    className="text-muted"
+                  >
+                    {bgText}
+                  </label>
+                </div>
+                {error && (
+                  <div style={labelStyling} className="pt-3">
+                    {error}
+                  </div>
+                )}
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={handleSubmit}
+                  style={buttonStyling}
+                  className="mt-2"
+                >
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Register
+export default Register;
