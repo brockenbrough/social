af9b70f42651695fc8d1400028f2e87db8377e30
Bryan Champagne <S0376997@salemstate.edu>
2022-11-29 11:59:07 -0500

    Commented out broken code

--- ui/src/App.js
+++ ui/src/App.js
@@ -16,7 +16,7 @@ import Login from './components/users/Login'
 import Signup from './components/register/Register'
 import Feed from './components/feed/Feed';
 import EditUserPage from "./components/users/editUserPage";
-import PublicProfile from "./components/users/PublicProfilePage";
+//import PublicProfile from "./components/users/PublicProfilePage";
 import PrivateUserProfile from "./components/users/PrivateUserProfile";
 import Test from "./Test";
 import FollowerList from "./components/following/followerListPage";
