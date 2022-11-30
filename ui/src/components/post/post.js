0e91bb9d9b31826c555f70aea9bd6160cf25cf15
Annie <77852190+adequitATSSU@users.noreply.github.com>
2022-11-30 10:09:52 -0500

    comment ui

--- ui/src/components/post/post.js
+++ ui/src/components/post/post.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Stack from 'react-bootstrap/Stack';
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack";
 
-const Post = ({username, content, date}) => {
-
-
-    return(
-      <div>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>{username}</Card.Title>
-                        <Card.Text>
-                            {content}
-                        </Card.Text>
-                        <div class = "text-center">
-                            <Button variant="outline-danger">Unlike</Button> 
-                        </div>
-
-                    </Card.Body>
-                </Card>
-           
-        </div>
-    )
+const Post = ({ username, content, date }) => {
+  return (
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{username}</Card.Title>
+          <Card.Text>{content}</Card.Text>
+          <div class="text-center">
+            <Button variant="outline-danger">Unlike</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Post;
