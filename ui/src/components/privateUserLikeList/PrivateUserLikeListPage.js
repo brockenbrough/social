b48558e7456a30928bb6e4cc5e00a1209a268ad0
Joseph Frongillo <j_frongillo@salemstate.edu>
2022-12-01 13:21:52 -0500

    mc

--- ui/src/components/privateUserLikeList/PrivateUserLikeListPage.js
+++ ui/src/components/privateUserLikeList/PrivateUserLikeListPage.js
@@ -10,43 +10,43 @@ import { UserContext } from '../../App';
 
 const PrivateUserLikeListPage = () => {
     //Setting active user
-    // const user = useContext(UserContext)
-    // const [post, setPost] = useState([])
+    const user = useContext(UserContext)
+    const [post, setPost] = useState([])
 
-    // useEffect(() => {
-    //     const fetchLikes = async () => {
-    //         try {
+    useEffect(() => {
+        const fetchLikes = async () => {
+            try {
                 
-    //             await api.get(`/${user.id}`).then(e => {
-    //                 /**
-    //                  * Iterates through the array of objects
-    //                  * For every like object, we want to find a post that matches the like's postId 
-    //                  */
-    //                 e.data.map(async (e) => {
-    //                     const res = await postURL.get(`/getPostById/${e.postId}`)
-    //                     //"...post" appeneds res.data to our post array to prevent overriting 
-    //                     setPost(e => [...e,res.data])
-    //                     //setPost(res.data)
-    //                 })
-    //             })
+                await api.get(`/${user.id}`).then(e => {
+                    /**
+                     * Iterates through the array of objects
+                     * For every like object, we want to find a post that matches the like's postId 
+                     */
+                    e.data.map(async (e) => {
+                        const res = await postURL.get(`/getPostById/${e.postId}`)
+                        //"...post" appeneds res.data to our post array to prevent overriting 
+                        setPost(e => [...e,res.data])
+                        //setPost(res.data)
+                    })
+                })
 
-    //         } catch (error) {
-    //             console.log(error.response.data);
-    //             console.log(error.response.status);
-    //             console.log(error.response.headers);
-    //         }
-    //     }
-    //     fetchLikes()
-    // }, [])
-    // console.log(user)
-    // return (
-    //     <div>
-    //         <h2>{user.username}'s liked posts:</h2>
-    //         {post.map(e => {
-    //            return <Post username ={e.username} content={e.content} date={e.date}/>
-    //         })}
-    //     </div>
-    // )
+            } catch (error) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+        }
+        fetchLikes()
+    }, [])
+    console.log(user)
+    return (
+        <div>
+            <h2>{user.username}'s liked posts:</h2>
+            {post.map(e => {
+               return <Post username ={e.username} content={e.content} date={e.date}/>
+            })}
+        </div>
+    )
 }
 
 export default PrivateUserLikeListPage
