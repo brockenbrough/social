13cb83af127c034b1775aac1138d8bd4e6a2e87a
jbtokam <85589835+jeanbTokam@users.noreply.github.com>
2022-12-05 02:16:02 -0500

    Fixed url Post-UI

--- ui/src/components/post/createPost.js
+++ ui/src/components/post/createPost.js
@@ -1,85 +1,104 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import Button from "react-bootstrap/Button";
-  const createPost = () => {
+import { useNavigate } from 'react-router-dom'
+
+const createPost = () => {
     const [state, setState] = useState({
-        content:'',
-        username:'',
-        postImage:'',
-        date:''
+        content: '',
+        username: '',
+        postImage: '',
+        date: ''
+    })
+    const navigate = useNavigate();
 
-  });
-    const { content, username,postImage} = state; 
-    const onChange = e => {
-        setState({ ...state, [e.target.name]: e.target.value });
-    };
-    const onSubmit = e => {
-        e.preventDefault();
-        axios.post('http://localhost:8083/posts/createPost', { content, username })
-        .then(res => {
-            console.log(res);
-            setState({
-                ...state,
-                content:'',
-                username:'',
-                postImage:'',
-                date:''
-
-            })
-        })
-        .catch(err => {
-            console.log(err);
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setState({
+            ...state,
+            [name]: value
         })
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const { content, username, postImage, date } = state
+        const post = {
+            content,
+            username,
+            postImage,
+            date
+        }
+        await axios.post('posts/createPost', post)
+        navigate('/')
+    }
+
     return (
         <div className="container">
-            <h1>Create Post</h1>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label className="text-muted">Content</label>
-                    <input
-                        onChange={onChange}
-                        value={content}
-                        type="text"
-                        name="content"
-                        className="form-control"
-                        placeholder="Write something..."
-                        required
-                    />
+            <div className="row">
+                <div className="col-md-6 mt-5 mx-auto">
+                    <form noValidate onSubmit={handleSubmit}>
+                        <h1 className="h3 mb-3 font-weight-normal">Create Post</h1>
+                        <div className="form-group">
+                            <label htmlFor="content">Content</label>
+                            <input type="text"
+
+                                className="form-control"
+                                name="content"
+                                placeholder="Enter Content"
+                                value={state.content}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text"
+
+                                className="form-control"
+                                name="username"
+                                placeholder="Enter Username"
+                                value={state.username}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="postImage">Post Image</label>
+                            <input type="text"
+
+                                className="form-control"
+                                name="postImage"
+                                placeholder="Enter Post Image"
+                                value={state.postImage}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="date">Date</label>
+                            <input type="text"
+
+                                className="form-control"
+                                name="date"
+                                placeholder="Enter Date"
+                                value={state.date}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-lg btn-primary btn-block"
+                        >
+                            Create Post
+                        </button>
+                    </form>
                 </div>
-                <div className="form-group">
-                    <label className="text-muted">Image</label>
-                    <input
-                        onChange={onChange}
-                        value={postImage}
-                        type="file"
-                        name="postImage"
-                        className="form-control"
-                        placeholder="Write something..."
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="text-muted">Username</label>
-                    <input
-                        onChange={onChange}
-                        value={username}
-                        type="text"
-                        name="username"
-                        className="form-control"
-                        placeholder="Your username"
-                        required
-                    />
-                </div>
-                <div>
-                    <Button  style={{marginTop:'1cm'}} variant="primary" type="submit">
-                        Create Post
-                    </Button>
-                </div>
-            </form>
+            </div>
         </div>
-    );
-};
+
+    )
+
+}
+
+
 
 
 
