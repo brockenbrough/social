307412d9d9b0b3106555eba763760772a0eb0495
Annie <77852190+adequitATSSU@users.noreply.github.com>
2022-12-06 13:21:19 -0500

    fixed

--- ui/src/components/comments/comment.js
+++ ui/src/components/comments/comment.js
@@ -24,7 +24,7 @@ const Comment = (props) => {
             <Button
               variant="primary"
               className="mx-1 my-1"
-              href={`/comments/editComments/${comment._id}`}
+              href={`/comments/editComment/${comment._id}`}
             >
               Edit
             </Button>
