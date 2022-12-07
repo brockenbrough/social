e2a7a5069fd18ee88c7df59e0a68f94a66151bfc
Allan Brockenbrough <brockenbrough@gmail.com>
2022-12-07 17:03:45 -0500

    Fixed delete contributor.

--- ui/src/components/project-notes/contributor.js
+++ ui/src/components/project-notes/contributor.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 
 const Contributor = (props) => {
     const  contributor  = props.record;
+
+    const deleteContributor = async (contributor) => {
+      axios.delete(`http://localhost:8095/project_notes/contributor/${contributor._id}`)
+          .then(response => {
+              alert('Contributor deleted.')
+          })
+          .catch(error => alert('Error deleting contributor'))
+    }
 
     return(
       <Card body outline color="success" className="mx-1 my-2" style={{ width: '30rem' }}>
@@ -15,6 +24,9 @@ const Contributor = (props) => {
               <div>{contributor.position}</div>
               <div>
                 <Button variant="primary" className="mx-1 my-1" href={`/project-notes/editContributor/${contributor._id}`} >Edit</Button>
+              </div>
+              <div>
+                <Button variant="primary" className="mx-1 my-1" onClick={() => deleteContributor(contributor)}>Delete</Button>
               </div>
             </Stack>
         </Card.Body>
