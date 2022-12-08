7712f49e862cd34deea5ab1f42ab11551df551a4
Marcus Uk <marcusuk91@gmail.com>
2022-12-08 13:28:23 -0500

    Update All Posts ui
    Added Clickability to username(May not actually link)
    Made the Design look better

--- ui/src/components/post/getAllPost.js
+++ ui/src/components/post/getAllPost.js
@@ -7,9 +7,11 @@ import moment from "moment";
 
 
 
+
 const getAllPost = () => {
   
     const [posts, setPosts] = useState([])
+
 
 
     const fetchPosts = async () => {
@@ -45,10 +47,10 @@ const getAllPost = () => {
             <h1>All Posts</h1>
             {posts.map((posts, index) => (
                 <div key={index}>
-                    <Card style={{ width: '18rem' , marginTop:'1cm', marginLeft:'21cm'}}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card style={{ width: '18rem' , marginTop:'1cm', marginLeft:'0cm',background:'aliceblue'}}>
+                        
                         <Card.Body>
-                            <Card.Title><h5>Username:</h5>{posts.username}</Card.Title>
+                            <Card.Title><h5>Username:</h5><Link to={'/publicprofilepage'}>{posts.username}</Link>{}</Card.Title>
                             <Card.Text><h3>Content:</h3>
                                 {posts.content}
                             </Card.Text>
@@ -58,11 +60,9 @@ const getAllPost = () => {
                         </Card.Body>
                     </Card>
                 </div>
+                
             ))}
         </div>
     )
 }
-  
-
-
 export default getAllPost
