d0f853e5706d2ec59796ba9db405bd919304d34c
Marcus Uk <marcusuk91@gmail.com>
2022-12-08 11:59:05 -0500

    Post Team Fix post.updatePost.js
    You can now update a Post, but does not redirect you to home page(yet)

--- post/server/routes/post.updatePost.js
+++ post/server/routes/post.updatePost.js
@@ -3,13 +3,11 @@ const router = express.Router();
 const newPostModel = require('../models/postModel')
 
 router.put("/updatePost/:postId", async (req, res) => { 
-    const { postId } = req.params;
-    const { content, username } = req.body;
-    newPostModel.findByIdAndUpdate
-    (postId, { content, username } , { new: true }).exec((err,this.post))
-    if (err) console.log(err);
-    res.json(post);     
+    const { postId } = req.params
 
+    const updatePost = newPostModel.findByIdAndUpdate(req.params.postId, req.body)
+        .then(post => res.json({msg: 'Post sucessfully updated'}))
+        .catch(err =>res.status(400).json({ error: 'Unable to update the Database' }));
 })
   
 module.exports = router;
