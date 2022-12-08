e6e0b7fb4a0e19afd57490837c8e72652878e6c1
Allan Brockenbrough <brockenbrough@gmail.com>
2022-12-08 12:10:10 -0500

    Preet: got delete working.

--- ui/src/components/comments/comment.js
+++ ui/src/components/comments/comment.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React from "react"; 
 import { Link } from "react-router-dom";
+import axios from 'axios'
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
 
 const Comment = (props) => {
   const comment = props.record;
+  const deleteComment = async (comment) => {
+    axios.delete(`http://localhost:8089/comments/comment/${comment._id}`)
+        .then(response => {
+            alert('Comment deleted.')
+        })
+        .catch(error => alert('Error deleting comment'))
+  }
 
   return (
     <Card
@@ -21,6 +29,13 @@ const Comment = (props) => {
             <h4>{comment.commentContent}</h4>
           </div>
           <div>
+          <Button
+              variant="primary"
+              className="mx-1 my-1"
+              href={`/comments/replyComment/${comment._id}`}
+            >
+              Reply
+            </Button>
             <Button
               variant="primary"
               className="mx-1 my-1"
@@ -28,6 +43,8 @@ const Comment = (props) => {
             >
               Edit
             </Button>
+            
+            <Button variant="primary" className="mx-1 my-1" onClick={() => deleteComment(comment)}>Delete</Button>
           </div>
         </Stack>
       </Card.Body>
