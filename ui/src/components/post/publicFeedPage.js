9c19a71b9d6a9fa4ea134e7bc7953b49db32656d
JWoodSSU <j_wood@salemstate.edu>
2022-12-11 18:32:35 -0500

    Removed unnecesary import

--- ui/src/components/post/publicFeedPage.js
+++ ui/src/components/post/publicFeedPage.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import getUserInfo from "../../utilities/decodeJwt";
 import Post from "./post";
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
