2e4286e55e1c42c60f7c54e0d158a87461bd543e
Monse Evelyn <77801300+ERODRIGUEZATSSU@users.noreply.github.com>
2022-12-13 13:32:23 -0500

    getall post implemented to PublicUser

--- ui/src/components/users/PublicUser.js
+++ ui/src/components/users/PublicUser.js
@@ -1,52 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import getUserInfo from '../../utilities/decodeJwt'
-import Comment from '../comments/comment'
-import Feed from '../feed/Feed'
-import PostList from '../post/feedPage'
-import CommentList from '../comments/commentListPage'
+import React, { useState, useEffect, useContext } from 'react';
+import { Image } from "react-bootstrap";
+import {Row, Col} from 'react-bootstrap';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import {UserContext} from "../../App"
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-// using axois 
+import moment from "moment";
+import getUserInfo from '../../utilities/decodeJwt';
+import Form from 'react-bootstrap/Form';
+import Post from '../post/post';
+import PostList from '../post/feedPage';
+
+
+
 const PublicUser = () => {
-    const [user, setUser] = useState({})
-    const navigate = useNavigate()
-   const [posts, setPosts] = useState([])
+	const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	const user = useContext(UserContext)
+	const username = getUserInfo().username
+	const [form, setValues] = useState({content : ""})
+	const [posts, setPosts] = useState([])
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        setUser(getUserInfo())
-    }, [])
-    if (!user) 
-    return (
-    <div>
-      <h3>You are on a public page <Link to={'/publicprofilepage'}>
-      <a href='#'>for some fun stuff click here</a></Link></h3></div>)
 
-    const { id, email, username, password } = user
-    if (!user)
-    return (
-      <>
-      <div>
-        <h3>
-           Please Login in{" "}
-          <Link to={'http://localhost:8093/comments/commentListPage'}>
-            <a href="#">here</a>
-          </Link>
-          </h3>
-          </div>
-      </>
-      
-    );
 
-    const fetchPosts = async () => {
-      const res = await axios.get(`http://localhost:8083/posts/getAllByUsername/${username}`)
-        .then(res => {
-          setPosts(res.data)
-        })
-        .catch(error => alert('error fetching data'))
-    }
+	const fetchPosts = async () => {
+	  const res = await axios.get(`http://localhost:8083/posts/getAllPosts/${posts}`)
+		  .then(res => {
+			  setPosts(res.data)
+		  })
+		  .catch(error => alert('error fetching data'))
+	}
+
+	useEffect(() => {
+		  fetchPosts()
+	}, [])
+
+
+return(
+	<div class="container">
+		<div class="col-md-12 text-center">
+
+<h1>{Post && Post.PostList}</h1>
+        
+			<div class="col-md-12 text-center">
+			</div>
+			<div class = "col-md-12 text-center">
+			</div>
+		</div>
+	
+<Card.Header>{Post && Post.PostList}</Card.Header>
+          <div>
+          <h3>All Posts</h3>
+				  <Row>
+			</Row>
+			<Card style={{ width: '5rem' }}></Card>
+   </div>
+   <div>
+            {posts.map((posts, index) => (
+                <div key={index}>
+                    <Card style={{ width: '18rem' , marginTop:'1cm', marginLeft:'.5cm',background:'aliceblue'}}>
+                        
+                        <Card.Body>
+                            <Card.Title><h5>User:</h5><Link to={'/publicprofilepage'}>{posts.username}</Link>{}</Card.Title>
+                                {posts.content}
+                            <p>{moment(posts.createdAt).format("MMM DD yyyy")}</p>
+                            
+                        </Card.Body>
+                    </Card>
+                </div>
+                
+            ))}
+   </div>
+  </div>
+)
+
 
 }
-
-
 
 export default PublicUser
